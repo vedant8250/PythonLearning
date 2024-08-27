c130faab631f0da0864c45e5cd9fabbdcd43f6ac
Vedant Magar <vedantm.2189@gmail.com>
2024-08-27 12:56:35 +0530

    add fundamentals.py

---
+++ Fundamentals/fundamentals.py
@@ -0,0 +1,37 @@
+# string
+name="vedant magar"
+
+# int
+age=21
+
+# boolean operators
+a=34
+b=53
+print(a>b)
+
+# boolean 2
+married = True
+# married = False
+
+# float
+a=67.23
+
+# complex number
+complex_num = 3 + 4j
+
+# character literals
+some_character="S"
+ 
+#  none
+value= None
+
+# Print
+print(type(a))
+print(married)
+print(name)
+print(age)
+print(type(name))
+print(complex_num)
+print(some_character)
+print(value)
+print(type(value))
