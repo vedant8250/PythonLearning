ad19dece3decefff5579a4fd4142265a72aa9291
Vedant Magar <vedantm.2189@gmail.com>
2024-09-09 10:53:28 +0530

    Adding python set operations and methods

--- Python Data Types/data_types.py
+++ Python Data Types/data_types.py
@@ -328,3 +328,376 @@ text = 'Python is fun'
 # find the index of is
 result = text.index('is')
 print(result)
+
+
+
+# Python Sets
+
+# create a set of integer type
+student_id = {112, 114, 116, 118, 115}
+print('Student ID:', student_id)
+
+# create a set of string type
+vowel_letters = {'a', 'e', 'i', 'o', 'u'}
+print('Vowel Letters:', vowel_letters)
+
+# create a set of mixed data types
+mixed_set = {'Hello', 101, -2, 'Bye'}
+print('Set of mixed data types:', mixed_set)
+
+
+# Create an empty set in python
+# create an empty set
+empty_set = set()
+
+# create an empty dictionary
+empty_dictionary = { }
+
+# check data type of empty_set
+print('Data type of empty_set:', type(empty_set))
+
+# check data type of dictionary_set
+print('Data type of empty_dictionary:', type(empty_dictionary))
+
+
+#Duplicate item in a set
+numbers = {2, 4, 6, 6, 2, 8}
+print(numbers)   # {8, 2, 4, 6}
+
+# Add and Update Set Items in Python
+numbers = {21, 34, 54, 12}
+print('Initial Set:',numbers)
+
+# using add() method
+numbers.add(32)
+print('Updated Set:', numbers) 
+
+
+#Update python set
+companies = {'Lacoste', 'Ralph Lauren'}
+tech_companies = ['apple', 'google', 'apple']
+
+# using update() method
+companies.update(tech_companies)
+print(companies)
+
+
+#Remove an element from a set
+languages = {'Swift', 'Java', 'Python'}
+print('Initial Set:',languages)
+
+# remove 'Java' from a set
+removedValue = languages.discard('Java')
+print('Set after remove():', languages)
+
+
+# Built in functions with set 
+# 1.all()
+boolean_list = ['True', 'True', 'True']
+
+# check if all elements are true
+result = all(boolean_list)
+print(result)
+
+# 2.any()
+boolean_list = ['True', 'False', 'True']
+
+# check if any element is true
+result = any(boolean_list)
+print(result)
+
+# 3.enumerate()
+# languages = ['Python', 'Java', 'JavaScript']
+
+# # enumerate the list
+# enumerated_languages = enumerate (languages)
+
+# # convert enumerate object to list
+# print(list(enumerated_languages))
+
+
+# 4.len()
+languages = ['Python', 'Java', 'JavaScript']
+
+length = len(languages)
+
+print(length)
+
+
+# 5.max()
+numbers = [9, 34, 11, -4, 27]
+
+# find the maximum number
+max_number = max(numbers)
+print(max_number)
+
+
+# 6.min()
+numbers = [9, 34, 11, -4, 27]
+
+# find the smallest number
+min_number = min(numbers)
+print(min_number)
+
+
+# 7.sorted()
+numbers = [4, 2, 12, 8]
+
+# sort the list in ascending order
+sorted_numbers = sorted(numbers)
+
+print(sorted_numbers)
+
+
+# 8.sum()
+marks = [65, 71, 68, 74, 61]
+
+# find sum of all marks
+total_marks = sum(marks)
+print(total_marks)
+
+
+
+# Iterate Over a Set in Python
+fruits = {"Apple", "Peach", "Mango"}
+
+# for loop to access each fruits
+for fruit in fruits: 
+    print(fruit)
+
+
+# Find number of set elements
+even_numbers = {2,4,6,8}
+print('Set:',even_numbers)
+
+# find number of elements
+print('Total Elements:', len(even_numbers))
+
+
+# Python set operations
+
+# Union of two sets
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {0, 2, 4}
+
+# perform union operation using |
+print('Union using |:', A | B)
+
+# perform union operation using union()
+print('Union using union():', A.union(B)) 
+
+
+# Set intersection
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {1, 2, 3}
+
+# perform intersection operation using &
+print('Intersection using &:', A & B)
+
+# perform intersection operation using intersection()
+print('Intersection using intersection():', A.intersection(B)) 
+
+
+# Difference between two sets
+# first set
+A = {2, 3, 5}
+
+# second set
+B = {1, 2, 6}
+
+# perform difference operation using &
+print('Difference using &:', A - B)
+
+# perform difference operation using difference()
+print('Difference using difference():', A.difference(B)) 
+
+
+# Set Symmetric Difference
+# first set
+A = {2, 3, 5}
+
+# second set
+B = {1, 2, 6}
+
+# perform difference operation using &
+print('using ^:', A ^ B)
+
+# using symmetric_difference()
+print('using symmetric_difference():', A.symmetric_difference(B)) 
+
+
+# Check if two sets are equal
+# first set
+A = {1, 3, 5}
+
+# second set
+B = {3, 5, 1}
+
+# perform difference operation using &
+if A == B:
+    print('Set A and Set B are equal')
+else:
+    print('Set A and Set B are not equal')
+
+
+
+# Other python set methods
+# 1.add()
+prime_numbers = {2, 3, 5, 7}
+
+# add 11 to prime_numbers
+prime_numbers.add(11)
+
+print(prime_numbers)
+
+# 2.clear()
+# set of prime numbers
+primeNumbers = {2, 3, 5, 7}
+
+# clear all elements
+primeNumbers.clear()
+
+print(primeNumbers)
+
+
+# 3.copy()
+numbers = {1, 2, 3, 4}
+
+# copies the items of numbers to new_numbers
+new_numbers = numbers.copy()
+
+print(new_numbers)
+
+# 4.difference()
+# sets of numbers
+A = {1, 3, 5, 7, 9}
+B = {2, 3, 5, 7, 11}
+
+# returns items present only in set A
+print(A.difference(B)) 
+
+# 5.difference update()
+# sets of numbers
+A = {1, 3, 5, 7, 9}
+B = {2, 3, 5, 7, 11}
+
+# computes A - B and updates A with the resulting set
+A.difference_update(B)
+
+print('A = ', A)
+
+
+# 6.discard()
+numbers = {2, 3, 4, 5}
+
+# removes 3 and returns the remaining set
+numbers.discard(3) 
+
+print(numbers)
+
+
+# 7.intersection()
+A = {2, 3, 5}
+B = {1, 3, 5}
+
+# compute intersection between A and B
+print(A.intersection(B))
+
+ 
+# 8.intersection update()
+A = {1, 2, 3, 4}
+B = {2, 3, 4, 5}
+
+# updates set A with the items common to both sets A and B
+A.intersection_update(B)
+
+print('A =', A)
+
+
+# 9.isdisjoint()
+A = {1, 2, 3, }
+B = {4, 5, 6}
+
+# checks if set A and set B are disjoint
+print(A.isdisjoint(B))
+
+# 10.issubset
+A = {1, 2, 3}
+B = {1, 2, 3, 4, 5}
+
+# all items of A are present in B
+print(A.issubset(B))
+
+# 11.issuperset
+A = {1, 2, 3, 4, 5}
+B = {1, 2, 3}
+C = {1, 2, 3}
+
+# Returns True
+print(A.issuperset(B))
+
+# Returns False
+print(B.issuperset(A))
+
+# Returns True
+print(C.issuperset(B))
+
+
+# 12.pop()
+A = {'a', 'b', 'c', 'd'}
+removed_item = A.pop()
+print(removed_item)
+
+
+# 13.remove()
+languages = {'Python', 'Java', 'English'}
+
+# remove English from the set
+languages.remove('English')
+
+print(languages)
+
+
+# 14.symmetric difference()
+A = {'a', 'b', 'c', 'd'}
+B = {'c', 'd', 'e' }
+
+# returns all items to result variable except the items on intersection
+result = A.symmetric_difference(B)
+print(result)
+
+
+# 15.symmetric difference update
+A = {'a', 'c', 'd'}
+B = {'c', 'd', 'e' }
+
+# updates A with the symmetric difference of A and B
+A.symmetric_difference_update(B)
+
+print(A)
+
+
+# 16.union()
+A = {2, 3, 5}
+B = {1, 3, 5}
+
+# compute union between A and B
+print('A U B = ', A.union(B))
+
+# 17.update()
+A = {'a', 'b'}
+B = {1, 2, 3}
+
+# updates A after the items of B is added to A
+A.update(B)
+
+print(A)
+
+
+# Python Dictionary
